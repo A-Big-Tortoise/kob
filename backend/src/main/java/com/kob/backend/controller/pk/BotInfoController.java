24ea850248bb4b8d6cae548f8c17e454884e5649
chen <2420211775@qq.com>
2022-08-10 11:24:35 +0800

    “前后端首次接通”

--- backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
+++ backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
@@ -3,12 +3,20 @@ package com.kob.backend.controller.pk;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/pk/")
+//@RequestMapping注解是用来映射请求的，
+// 即指明处理器可以处理哪些URL请求，该注解既可以用在类上，也可以用在方法上。
 public class BotInfoController {
+    @RequestMapping("getbotinfo/")
 
-    @RequestMapping("getbotInfo/")
-    public String getBotInfo(){
-        return "hhhhhhh";
+    public Map<String,String> getBotInfo(){
+        Map<String, String> bot1 = new HashMap<>();
+        bot1.put("name", "tiger");
+        bot1.put("rating", "1500");
+        return bot1;
     }
 }
